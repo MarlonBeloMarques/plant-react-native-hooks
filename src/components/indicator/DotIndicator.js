2c27272be325ca0d9036cfda86c1c8310cd9eb3b
MarlonMarques <marlon.belohd@gmail.com>
2020-02-14 16:38:49 -0400

    test (runner): added indicator component

---
+++ src/components/indicator/DotIndicator.js
@@ -0,0 +1,69 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Animated, Easing } from "react-native";
+import Indicator from "./Indicator";
+
+export default function DotIndicator(props) {
+  
+  const defaultProps = {
+    animationEasing: Easing.inOut(Easing.ease),
+    color: "rgb(0, 0, 0)",
+    count: 4,
+    size: 16 
+  };
+
+  const propTypes = {
+    ...Indicator.propTypes,
+
+    color: PropTypes.string,
+    size: PropTypes.number
+  };  
+
+  function renderComponent({ index, count, progress }) {
+    let { size, color: backgroundColor } = props;
+
+    let style = {
+      width: size,
+      height: size,
+      margin: size / 3,
+      borderRadius: size / 2,
+      backgroundColor,
+      transform: [
+        {
+          scale: progress.interpolate({
+            inputRange: [
+              0.0,
+              (index + 0.5) / (count + 1),
+              (index + 1.0) / (count + 1),
+              (index + 1.5) / (count + 1),
+              1.0
+            ],
+            outputRange: [1.0, 1.36, 1.56, 1.06, 1.0]
+          })
+        }
+      ]
+    };
+
+    return <Animated.View style={style} {...{ key: index }} />
+  }
+
+  const { style, ...props } = this.props;
+  
+  return (
+    <Indicator
+      style={[styles.container, style]}
+      renderComponent={this.renderComponent}
+      {...props}
+    />
+  )
+  
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
+  }
+}
---
+++ src/components/indicator/Indicator.js
@@ -0,0 +1,117 @@
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
+import { Animated, Easing } from "react-native";
+import RN from "react-native/package";
+
+const [major, minor] = RN.version.split(".").map(item => Number(item));
+const hasLoopSupport = !major && minor >= 45;
+
+export default function Indicator(props) {
+  const defaultProps = {
+    animationEasing: Easing.linear,
+    animationDuration: 1200,
+
+    animating: true,
+    interaction: true,
+
+    count: 1
+  };
+
+  const propTypes = {
+    animationEasing: PropTypes.func,
+    animationDuration: PropTypes.number,
+
+    animating: PropTypes.bool,
+    interaction: PropTypes.bool,
+
+    renderComponent: PropTypes.func,
+    count: PropTypes.number
+  };
+
+  const [isProgress, setIsProgress] = useState(new Animated.Value(0))
+
+  mounted = false;
+
+  function startAnimation({ finished } = {}) {
+    let progress = isProgress
+    let { interaction, animationEasing, animationDuration } = props;
+
+    if (!mounted || false === finished) {
+      return;
+    }
+
+    let animation = Animated.timing(progress, {
+      duration: animationDuration,
+      easing: animationEasing,
+      useNativeDriver: true,
+      isInteraction: interaction,
+      toValue: 1
+    });
+
+    if (hasLoopSupport) {
+      Animated.loop(animation).start();
+    } else {
+      progress.setValue(0);
+      animation.start(startAnimation);
+    }
+
+    setIsProgress(animation)
+  }
+
+  function stopAnimation() {
+    let { animation } = isProgress;
+
+    if (null == animation) {
+      return;
+    }
+
+    animation.stop();
+
+    setIsProgress({ animation: null });
+  }
+
+  useEffect(() => {
+    let { animating } = props;
+
+    mounted = true;
+
+    if (animating) {
+      startAnimation();
+    }
+
+    return () => {
+      mounted = false;
+    }
+  }, [])
+
+  useEffect(() => {
+    let { animating } = props;
+
+    if (animating ^ props.animating) {
+      if (animating) {
+        stopAnimation();
+      } else {
+        startAnimation();
+      }
+    }
+  })
+
+  function renderComponent(undefined, index) {
+    let { progress } = isProgress
+    let { renderComponent, count } = props;
+
+    if (typeof renderComponent === "function") {
+      return renderComponent({ index, count, progress });
+    } else {
+      return null;
+    }
+  }
+
+  let { count, ...props } = props;
+
+  return (
+    <Animated.View {...props}>
+      {Array.from(new Array(count), renderComponent)}
+    </Animated.View>
+  );
+}
