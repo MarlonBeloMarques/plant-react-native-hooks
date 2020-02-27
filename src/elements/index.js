5fab3ba19b2393b6fe80e89ea9e191add088ee46
MarlonMarques <marlon.belohd@gmail.com>
2020-02-27 09:04:23 -0400

    indicator export bug fix

--- src/elements/index.js
+++ src/elements/index.js
@@ -2,11 +2,13 @@ import Block from './Block'
 import Text from './Text' 
 import Button from './Button' 
 import Input from './Input'
-
+import { DotIndicator, BarIndicator } from './indicator'
 
 export {
   Block,
   Text,
   Button,
-  Input
+  Input,
+  DotIndicator,
+  BarIndicator
 }
--- src/elements/indicator/index.js
+++ src/elements/indicator/index.js
@@ -1,2 +1,10 @@
 export * from "./DotIndicator";
 export * from "./BarIndicator";
+
+import DotIndicator from './DotIndicator'
+import BarIndicator from './BarIndicator'
+
+export {
+  DotIndicator,
+  BarIndicator
+}
